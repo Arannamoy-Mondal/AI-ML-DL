ee3843daebf022b6d3901c175abb358b093842ac
Arannamoy Mondal <arannamoymondal13@gmail.com>
2026-03-22 05:51:04 +0600

    upd:add Streamlit.py

---
+++ Computer_Vision/YOLO/streamlit.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.title("Streamlit intro")
+st.write("This is just warm up for this project")
+name=st.text_input("hello")
+if name:
+    st.write(f"Welcome, {name}!")
+
+age=st.slider(f"",0,100)
+
+st.write(f"Your age {age}")
+gender=st.radio("Gender",("Male","Female"))
+tech=st.multiselect("Tech:",("Spring","Django",".Net","Spring Boot","Spring Reactive","DRF"
+                             "Tensorflow","Pytorch"))
+interest=st.multiselect("Interest",("Microservices","Fintech","AI","ML"))
+
+@st.dialog("Profile")
+def open_modal():
+    st.write(f"Your name: {name}")
+    st.write(f"Your age: {age}")
+    st.write(f"Gender: {gender}")
+    st.write(f"Interest: {interest}")
+if st.button("Create Profile"):
+    open_modal()
+
+
